31a01022decb9acd0cfc5b30e9cc239b658b250d
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-11-13 15:47:48 +0300

    Added icons from Material UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Home/Hero"
+import TopJobs from "../components/Home/NewJobs"
 
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Hero className="bg-white w-full h-1/2 flex flex-row overflow-auto"/>
-      <div className="grow">
+      <Hero className="bg-white w-full h-64 flex flex-row overflow-auto"/>
+      <div className="grow ">
+        <TopJobs />
       </div>
     </div>
   )
