6000b846ba7bc130657d5a86b2abd3d6579d1c52
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-11-16 02:18:42 +0300

    To be completed tommorow. Navbar styles

--- src/components/Navbar/NavLinkList.tsx
+++ src/components/Navbar/NavLinkList.tsx
@@ -8,12 +8,12 @@ const NavLinkList = ({ open }: PropTypes) => {
   return (
     <>
       <ul
-       className={`${open ? '' : 'hidden md:flex'} justify-around gap-[4rem]`}
+       className={`${open ? 'top-[5rem] bottom-0 left-0 right-0 bg-white w-full h-full translate-x-0 transition-all duration-200 ease-linear' : 'hidden fixed translate-x-full md:translate-x-0 transition-all duration-200 ease-linear md:static md:flex'} justify-around gap-[4rem] font-bold text-xl`}
       >
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/locums'>Locums</NavLink></li>
-        <li><NavLink to='/register'>About</NavLink></li>
-        <li><NavLink to='/register'>Register</NavLink></li>
+        <li><NavLink to='/' className={({isActive}) => isActive ? 'text-purple-800' : ''}>Home</NavLink></li>
+        <li><NavLink to='/locums' className={({isActive}) => isActive ? 'text-purple-800' : ''}>Locums</NavLink></li>
+        <li><NavLink to='/about' className={({isActive}) => isActive ? 'text-purple-800' : ''}>About</NavLink></li>
+        <li><NavLink to='/register' className={({isActive}) => isActive ? 'text-purple-800' : ''}>Register</NavLink></li>
       </ul>
     </>
   );
