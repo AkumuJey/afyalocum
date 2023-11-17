2f898affa76747ad8eb0748525a2a53a5065fd8f
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-11-18 00:19:46 +0300

    Will complete tomorrow

--- src/components/Locums/NewLocumForm.tsx
+++ src/components/Locums/NewLocumForm.tsx
@@ -7,12 +7,26 @@ import {
     // Button,
     // Avatar,
   } from "@mui/material";
-
+  import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+  import { MultiInputDateTimeRangeField } from '@mui/x-date-pickers-pro/MultiInputDateTimeRangeField';
+  import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+  import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 const NewLocumForm = () => {
     const ariaLabel = { "aria-label": "description" };
     const handleInputChange = () => {}
   return (
     <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['MultiInputDateTimeRangeField ']}>
+      <MultiInputDateTimeRangeField
+          slotProps={{
+            textField: ({ position }) => ({
+              label: position === 'start' ? 'Check-in' : 'Check-out',
+            }),
+          }}
+        />
+      </DemoContainer>
+      </LocalizationProvider>
         <InputLabel
             htmlFor="name"
             sx={{
