62b0363f44c58f06cf3f478012a958e8d724c6d1
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-11-26 18:07:53 +0300

    Name displayed and dedit done

---
+++ src/components/Profile/NameProfile.tsx
@@ -0,0 +1,96 @@
+
+
+import {
+ Grid,
+ Typography,
+ Button,
+ Input,
+ Box,
+ InputLabel,
+} from "@mui/material";
+import { useState, FormEvent } from "react";
+
+const Name = () => {
+ // State to manage the editable status of the component
+ const [isEditable, setIsEditable] = useState(false);
+
+ // Function to handle form submission
+ const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+ };
+
+ // Render the component based on the editable status
+ return (
+    <>
+      {/* If the component is not editable, display the name and an edit button */}
+      {!isEditable && (
+        <Grid container justifyContent={`space-between`} spacing={2}>
+          <Grid item>
+            <Typography fontWeight={`bold`}>Name: </Typography>
+            <Typography>Akumu Joseph Owino</Typography>
+          </Grid>
+          <Grid item>
+            <Button onClick={() => setIsEditable(true)}>Edit</Button>
+          </Grid>
+        </Grid>
+      )}
+
+      {/* If the component is editable, display a form to edit the name */}
+      {isEditable && (
+        <Box
+          component={`form`}
+          onSubmit={handleSubmit}
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "0.5rem",
+          }}
+        >
+          <Box component={`div`} sx={{width: "100%"}}>
+            <InputLabel
+              htmlFor="name"
+              sx={{
+                fontWeight: "bold",
+                color: "black",
+                width: "100%",
+                textAlign: "left",
+              }}
+            >
+              Name
+            </InputLabel>
+            <Input id="name" name="name" autoComplete="off" sx={{width: "100%",}}/>
+          </Box>
+          <Box
+            component={`div`}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              gap: "2rem",
+              width: "100%"
+            }}
+          >
+            <Button type="submit" color="primary" variant="contained">
+              Save
+            </Button>
+            <Button
+              type="button"
+              color="secondary"
+              onClick={() => setIsEditable(false)}
+              variant="outlined"
+            >
+              Cancel
+            </Button>
+          </Box>
+        </Box>
+      )}
+    </>
+ );
+};
+
+export default Name;
+//
+//In this code, we have a `Name` component that displays a user's name. When the "Edit" button is clicked, the component becomes editable, allowing the user to change their name. The changes are then saved using the "Save" button. The "Cancel" button reverts the component to its non-editable state..</s>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,27 +1,23 @@
 import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import UserAvatar from "../components/Navbar/UserAvatar";
+import NameProfile from "../components/Profile/NameProfile"
 
 const Profile = () => {
   return (
     <>
       <Box
         component={`div`}
-        className="w-[95%] md:w-[50%] valid-height flex flex-col justify-start text-center gap-[4rem] py-5"
+        className="w-[95%] max-w-lg valid-height flex flex-col justify-start gap-[4rem] py-5"
       >
         <Typography variant="h3">Profile</Typography>
-        <Paper elevation={2} sx={{ padding: 2 }}>
+        <Paper elevation={2} sx={{ padding: 2, }}>
           <Grid container justifyContent={`space-between`}>
             <Grid item xs={12} sm={6} md={4} lg={3}>
               <UserAvatar />
             </Grid>
             <Button sx={{ height: "auto" }}>Edit</Button>
           </Grid>
-          <Grid container justifyContent={`space-between`}>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <Typography>Name: </Typography>
-            </Grid>
-            <Button>Edit</Button>
-          </Grid>
+          <NameProfile />
           <Grid container justifyContent={`space-between`}>
             <Grid item xs={12} sm={6} md={4} lg={3}>
               <Typography>Description: </Typography>
