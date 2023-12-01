42a81e0f84dfb2aef607c00c8e2a2692e89c94d4
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-12-01 23:43:51 +0300

    Single display page done

--- src/components/Locums/SingleLocum.tsx
+++ src/components/Locums/SingleLocum.tsx
@@ -1,17 +1,29 @@
-
-import { Box, IconButton, Paper } from "@mui/material";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-
+import { Box, Button, Paper, Typography } from "@mui/material";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 interface Props {
-    onClose: () => void;
+  onClose: () => void;
 }
-const SingleLocum = ({onClose} : Props) => {
+const SingleLocum = ({ onClose }: Props) => {
   return (
     <>
-      <Paper elevation={3}>
+      <Paper elevation={3} sx={{ display: "relative", padding: "1rem" }}>
+        <Button
+          variant="contained"
+          startIcon={<ArrowBackIosIcon />}
+          onClick={onClose}
+          sx={{ top: "0.2rem", left: "0.2rem" }}
+        >
+          Back
+        </Button>
         <Box component={`div`}>
-            <IconButton onClick={onClose}><ArrowBackIosIcon /> Back</IconButton>
+          <Typography variant="h6">Person X</Typography>
+          <Typography variant="body1">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            temporibus quis omnis illum, illo est, cupiditate cum, excepturi
+            quidem dolore eveniet aspernatur similique dignissimos iure. Natus
+            quasi quod eos facilis?
+          </Typography>
         </Box>
       </Paper>
     </>
