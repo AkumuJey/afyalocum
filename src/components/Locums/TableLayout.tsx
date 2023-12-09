dd962dd6f544a243894d0810f36ae2102c2c92d0
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-12-09 16:48:46 +0300

    Styled skeleton for table display

--- src/components/Locums/TableLayout.tsx
+++ src/components/Locums/TableLayout.tsx
@@ -1,76 +1,122 @@
-import { Box } from "@mui/material";
+import { Box, Skeleton } from "@mui/material";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import { generateRandomData } from "./dummyData";
+import { useEffect, useState } from "react";
 
-interface Props{
-  onClick: () => void
+interface Props {
+  onClick: () => void;
 }
-const TableLayout = ({onClick} : Props) => {
+const TableLayout = ({ onClick }: Props) => {
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 90 },
     {
+      field: "fullName",
+      headerName: "Title",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 130,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+    {
       field: "firstName",
-      headerName: "First name",
-      width: 150,
+      headerName: "Location",
+      width: 120,
       editable: true,
-      pinnable: true
+      pinnable: true,
     },
     {
       field: "lastName",
-      headerName: "Last name",
-      width: 150,
+      headerName: "Start",
+      width: 120,
       editable: true,
     },
     {
       field: "age",
-      headerName: "Age",
+      headerName: "Rate KSh/Hr",
       type: "number",
-      width: 110,
+      width: 90,
       editable: true,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
     },
   ];
 
-  
+  const [isLoading, setLoading] = useState(true);
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    // Simulating data fetching delay
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        // Simulating API call delay
+        setTimeout(() => {
+          const data = generateRandomData();
+          setRows(data);
+          setLoading(false);
+        }, 5000); // Adjust the timeout to simulate data loading
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
 
- const rows = generateRandomData();
+    fetchData();
+  }, []);
   return (
-    <Box
-      sx={{
-        height: 400,
-        overflow: "auto",
-        width: {
-          xs: "100%",
-          md: "60%",
-          marginLeft: "auto",
-          marginRight: "auto",
-        },
-      }}
-    >
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 10,
-            },
+    <>
+      <Box
+        sx={{
+          height: 400,
+          overflow: "auto",
+          width: {
+            xs: "100%",
+            md: "60%",
+            marginLeft: "auto",
+            marginRight: "auto",
           },
         }}
-        pageSizeOptions={[10]}
-        checkboxSelection
-        disableRowSelectionOnClick
-        onRowClick={onClick}
-      />
-    </Box>
+      >
+        {isLoading ? (
+          Array.from({ length: 7 }, (_, index) => (
+            <Box
+              key={index}
+              sx={{
+                borderBottom: "1px solid #ddd",
+                display: "flex",
+                alignItems: "center",
+                padding: "0.5rem 0",
+              }}
+            >
+              <Skeleton
+                variant="rectangular"
+                animation="wave"
+                sx={{
+                  flex: 1,
+                  height: "2rem",
+                  borderRadius: "4px",
+                  marginLeft: "1rem",
+                }}
+              />
+            </Box>
+          ))
+        ) : (
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 10,
+                },
+              },
+            }}
+            pageSizeOptions={[10]}
+            checkboxSelection
+            disableRowSelectionOnClick
+            onRowClick={onClick}
+          />
+        )}
+      </Box>
+    </>
   );
 };
 
