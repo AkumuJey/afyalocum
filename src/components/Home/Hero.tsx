dce341313801d03e486efec481168ba96700ad0c
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-01-22 14:56:58 +0300

    Modified home page

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,13 +1,17 @@
 import { Button, Container, Grid, Typography } from "@mui/material";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Hero = () => {
+  const { currentUser } = useContext(AuthContext);
   const navigate = useNavigate();
   const handleClick = (direction: string) => {
     navigate(direction);
   };
+
   return (
-    <Container maxWidth="md" sx={{paddingTop: '2rem'}}>
+    <Container maxWidth="md" sx={{ paddingTop: "2rem" }}>
       <Typography variant="h3" component="h1" gutterBottom textAlign={`center`}>
         Connect with Healthcare Anytime, Anywhere
       </Typography>
@@ -20,16 +24,26 @@ const Hero = () => {
         <Grid container justifyContent={`space-around`}>
           <Grid item>
             <Button
-              variant="contained"
+              variant="outlined"
               color="primary"
-              onClick={() => handleClick(`/register`)}
+              onClick={() =>
+                handleClick(
+                  `${currentUser ? "/locums/create-new" : "/register"}`
+                )
+              }
             >
-              Get Started
+              {currentUser ? "Create New Locum" : "Get Started"}
             </Button>
           </Grid>
           <Grid item>
-            <Button variant="contained" color="primary" onClick={() => handleClick(`about`)}>
-              Learn More
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() =>
+                handleClick(`${currentUser ? "/locums" : "/about"}`)
+              }
+            >
+              {currentUser ? "View Listed Locums" : "Learn More"}
             </Button>
           </Grid>
         </Grid>
