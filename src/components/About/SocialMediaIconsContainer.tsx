697b804604793068661af6c0186588b33606e905
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-01-28 23:58:37 +0300

    Refactoring social media icons container

---
+++ src/components/About/SocialMediaIconsContainer.tsx
@@ -0,0 +1,46 @@
+import { Facebook, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import { motion } from "framer-motion";
+
+const SocialMediaIconsContainer = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.2 } },
+  };
+  const iconVariants = {
+    hidden: { opacity: 0, rotate: -180, y: 20 },
+    visible: { opacity: 1, rotate: 0, y: 0 },
+  };
+
+  return (
+    <motion.div
+      className={`w-fit my-2 p-2 flex justify-center items-center gap-5 `}
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+    >
+      <motion.div variants={iconVariants}>
+        <IconButton>
+          <Facebook sx={{ color: "blue", fontSize: "2rem" }} />
+        </IconButton>
+      </motion.div>
+      <motion.div variants={iconVariants}>
+        <IconButton>
+          <Twitter sx={{ color: "black", fontSize: "2rem" }} />
+        </IconButton>
+      </motion.div>
+      <motion.div variants={iconVariants}>
+        <IconButton>
+          <Instagram sx={{ color: "red", fontSize: "2rem" }} />
+        </IconButton>
+      </motion.div>
+      <motion.div variants={iconVariants}>
+        <IconButton>
+          <LinkedIn sx={{ color: "blue", fontSize: "2rem" }} />
+        </IconButton>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default SocialMediaIconsContainer;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,51 +1,13 @@
-import { Facebook, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
-import { motion } from "framer-motion";
 import AboutContentsArrayDisplay from "../components/About/AboutContentsArrayDisplay";
+import SocialMediaIconsContainer from "../components/About/SocialMediaIconsContainer";
 import RouterAnimation from "./RouterAnimation";
 
 const About = () => {
-  
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { staggerChildren: 0.2,} },
-  };
-  const iconVariants = {
-    hidden: { opacity: 0, rotate: -180, y: 20 },
-    visible: { opacity: 1, rotate: 0, y: 0 },
-  };
-
   return (
     <RouterAnimation>
       <div className="flex flex-col items-center valid-height py-[2rem]">
         <AboutContentsArrayDisplay />
-        <motion.div
-          className={`w-fit my-2 p-2 flex justify-center items-center gap-5 `}
-          initial="hidden"
-          animate="visible"
-          variants={containerVariants}
-        >
-          <motion.div variants={iconVariants}>
-            <IconButton>
-              <Facebook sx={{ color: "blue", fontSize: "2rem" }} />
-            </IconButton>
-          </motion.div>
-          <motion.div variants={iconVariants}>
-            <IconButton>
-              <Twitter sx={{ color: "black", fontSize: "2rem" }} />
-            </IconButton>
-          </motion.div>
-          <motion.div variants={iconVariants}>
-            <IconButton>
-              <Instagram sx={{ color: "red", fontSize: "2rem" }} />
-            </IconButton>
-          </motion.div>
-          <motion.div variants={iconVariants}>
-            <IconButton>
-              <LinkedIn sx={{ color: "blue", fontSize: "2rem" }} />
-            </IconButton>
-          </motion.div>
-        </motion.div>
+        <SocialMediaIconsContainer />
       </div>
     </RouterAnimation>
   );
