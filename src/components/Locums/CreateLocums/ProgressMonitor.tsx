e26683fe9eacd8c39cc4ab4170c9e10a2b13eb9c
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-01-30 23:16:18 +0300

    Began refactoring of page two

--- src/components/Locums/CreateLocums/ProgressMonitor.tsx
+++ src/components/Locums/CreateLocums/ProgressMonitor.tsx
@@ -1,16 +1,19 @@
 import { LinearProgress } from "@mui/material";
-interface PropTypes{
-    step: number
+interface PropTypes {
+  step: number;
 }
 
-const ProgressMonitor = ({step}: PropTypes) => {
+const ProgressMonitor = ({ step }: PropTypes) => {
   return (
-    <><LinearProgress
-    value={(step / 2) * 100}
-    variant="determinate"
-    sx={{ height: "0.5rem", borderRadius: "0.5rem", mb: "0.5rem" }}
-  /></>
-  )
-}
+    <>
+    <div>Step {step + "/" + 2}</div>
+      <LinearProgress
+        value={(step / 2) * 100}
+        variant="determinate"
+        sx={{ height: "0.5rem", borderRadius: "0.5rem", mb: "0.5rem" }}
+      />
+    </>
+  );
+};
 
-export default ProgressMonitor
+export default ProgressMonitor;
