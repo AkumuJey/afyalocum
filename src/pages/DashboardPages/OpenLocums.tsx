5842f626a286e088c8a6236d22a7e1a935e68506
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-04 19:14:13 +0300

    refactored open and booked

--- src/pages/DashboardPages/OpenLocums.tsx
+++ src/pages/DashboardPages/OpenLocums.tsx
@@ -1,42 +1,59 @@
 import { Skeleton } from "@mui/material";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, DocumentData, onSnapshot, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { SubmittedLocum } from "../../components/Dashboard/CreateLocums/hooks/useJobForm";
 import LocumCard from "../../components/Dashboard/LocumCard";
 import { db } from "../../firebase/firebase";
+import { Unsubscribe } from "firebase/auth";
 
 const OpenLocums = () => {
   const [locums, setLocums] = useState<SubmittedLocum[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
   useEffect(() => {
-    setLoading(true);
-    const locumsCollection = collection(db, "locums");
-    const unsubscribe = onSnapshot(locumsCollection, (snapshot) => {
-      const locumsArray: SubmittedLocum[] = [];
-      snapshot.docs.forEach((doc) => {
-        if (!doc.data().completed && !doc.data().booked) {
-          const data = doc.data() as SubmittedLocum;
-          data.id = doc.id;
-          if (!doc.data().closed) {
-            locumsArray.push(data);
-          }
-        }
-      });
-      setLoading(false);
-      setLocums([...locumsArray]);
-    });
-    return () => unsubscribe();
+    const fetchOpenLocums = () => {
+      try {
+        setLoading(true);
+        const locumsCollection = collection(db, "locums");
+        const bookedFilter = where("booked", "==", false);
+        const completedFilter = where("completed", "==", false);
+        const openLocumsCollection = query(
+          locumsCollection,
+          bookedFilter,
+          completedFilter
+        );
+        const unsubscribe = onSnapshot(openLocumsCollection, (snapshot) => {
+          const locumsArray = snapshot.docs
+            .map((doc) => {
+              const data = doc.data() as SubmittedLocum;
+              return { ...data, id: doc.id };
+            })
+            .filter((doc: DocumentData) => !doc.closed);
+          setLocums(locumsArray);
+        });
+        return unsubscribe;
+      } catch (_error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+        setError(false);
+      }
+    };
+    const unsubscribe: Unsubscribe | undefined = fetchOpenLocums();
+    if (unsubscribe) {
+      return () => unsubscribe();
+    }
   }, []);
-
   
   return (
     <>
       <div className="flex gap-[1.5rem] flex-wrap p-[1.5rem] justify-start w-[95%] md:w-4/4 mx-auto">
-        {error ? (
-          <div>Error fetching locums</div>
-        ) : (<>
-        {loading ? (
+        {error && (
+          <>
+            <div>Error Loading locums</div>
+          </>
+        )}
+        {loading &&
           Array(3)
             .fill("key")
             .map((item, index) => (
@@ -48,16 +65,14 @@ const OpenLocums = () => {
                     height: 180,
                     borderRadius: 3,
                   }}
-                  key={`${item + index}`}
+                  key={item + index}
                 />
               </>
-            ))
-        ) : locums.length === 0 ? (
-          <p>No locums available</p>
-        ) : (
-          locums.map((locum) => <LocumCard key={locum.id} locum={locum} />)
-        )}
-        </>)}
+            ))}
+        {!loading && locums.length === 0 && <p>No locums available</p>}
+        {!loading &&
+          locums.length > 0 &&
+          locums.map((locum) => <LocumCard key={locum.id} locum={locum} />)}
       </div>
     </>
   );
