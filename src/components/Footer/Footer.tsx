b2362cc0559d964c877a925a29348152dd5cdba4
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-05 23:59:50 +0300

    edits to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,12 @@
 const Footer = () => {
   return (
     <div>
-      
+      <div>
+        <div>Afya Locum</div>
+        <div>
+          <div></div>
+        </div>
+      </div>
     </div>
   )
 }
