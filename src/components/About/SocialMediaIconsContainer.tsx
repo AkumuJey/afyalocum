af67a76002d5a64299e1e1f33d57f2c3fbf7dd34
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-10 17:16:55 +0300

    Improved anchor tags in the about page

--- src/components/About/SocialMediaIconsContainer.tsx
+++ src/components/About/SocialMediaIconsContainer.tsx
@@ -1,15 +1,14 @@
 import { Facebook, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
 import { motion } from "framer-motion";
 
 const SocialMediaIconsContainer = () => {
   const containerVariants = {
     hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { staggerChildren: 0.3, } },
+    visible: { opacity: 1, transition: { staggerChildren: 0.3 } },
   };
   const iconVariants = {
     hidden: { opacity: 0, rotate: -180, y: 20 },
-    visible: { opacity: 1, rotate: 0, y: 0, },
+    visible: { opacity: 1, rotate: 0, y: 0 },
   };
 
   return (
@@ -19,26 +18,34 @@ const SocialMediaIconsContainer = () => {
       animate="visible"
       variants={containerVariants}
     >
-      <motion.div variants={iconVariants}>
-        <IconButton>
-          <Facebook sx={{ color: "blue", fontSize: "2rem" }} />
-        </IconButton>
-      </motion.div>
-      <motion.div variants={iconVariants}>
-        <IconButton>
-          <Twitter sx={{ color: "black", fontSize: "2rem" }} />
-        </IconButton>
-      </motion.div>
-      <motion.div variants={iconVariants}>
-        <IconButton>
-          <Instagram sx={{ color: "red", fontSize: "2rem" }} />
-        </IconButton>
-      </motion.div>
-      <motion.div variants={iconVariants}>
-        <IconButton>
-          <LinkedIn sx={{ color: "blue", fontSize: "2rem" }} />
-        </IconButton>
-      </motion.div>
+      <motion.a
+        variants={iconVariants}
+        href="http://localhost:5173/dashboard"
+        target="_blank"
+      >
+        <Facebook sx={{ color: "blue", fontSize: "2rem" }} />
+      </motion.a>
+      <motion.a
+        variants={iconVariants}
+        href="http://localhost:5173/dashboard"
+        target="_blank"
+      >
+        <Twitter sx={{ color: "black", fontSize: "2rem" }} />
+      </motion.a>
+      <motion.a
+        variants={iconVariants}
+        href="http://localhost:5173/dashboard"
+        target="_blank"
+      >
+        <Instagram sx={{ color: "red", fontSize: "2rem" }} />
+      </motion.a>
+      <motion.a
+        variants={iconVariants}
+        href="http://localhost:5173/dashboard"
+        target="_blank"
+      >
+        <LinkedIn sx={{ color: "blue", fontSize: "2rem" }} />
+      </motion.a>
     </motion.div>
   );
 };
--- src/components/About/useAboutContents.ts
+++ src/components/About/useAboutContents.ts
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
 
 export interface Content {
-    id: number;
-    title: string;
-    body: string;
-  }
+  id: number;
+  title: string;
+  body: string;
+}
 
 const useAboutContents = (): Content[] => {
-  const [aboutContents, setAboutContents] = useState<Content[] >([]);
+  const [aboutContents, setAboutContents] = useState<Content[]>([]);
   useEffect(() => {
     const fetchData = () => {
       const data = [
         {
           id: 1,
           title: "Mission",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque molestias est reiciendis et. Tempora, non officia ipsam quam iste animi architecto corrupti est fugit! Aliquid, alias voluptas. Corporis totam eligendi perferendis error, eaque quam fugiat beatae vero laudantium a ut facere! Facere officia ad, accusamus repudiandae et nesciunt natus ab deserunt accusantium itaque voluptatum",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, non officia ipsam quam iste animi architecto corrupti est fugit!",
         },
         {
           id: 2,
           title: "Vision",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque molestias est reiciendis et. Tempora, non officia ipsam quam iste animi architecto corrupti est fugit! Aliquid, alias voluptas. Corporis totam eligendi perferendis error, eaque quam fugiat beatae vero laudantium a ut facere! Facere officia ad, accusamus repudiandae et nesciunt natus ab deserunt accusantium itaque voluptatum",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque molestias est reiciendis et.",
         },
       ];
       setAboutContents(data);
