0b123af9c9452aaee3b139d42e6967c1ba2c1430
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-17 22:43:09 +0300

    Refactored routes page by moving dashboard children to variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouteObject, RouterProvider } from "react-router-dom";
 const LayoutDash = lazy(() => import("./Layout/LayoutDash"));
 const OpenLocums = lazy(() => import("./pages/DashboardPages/OpenLocums"));
 const SingleLocumPage = lazy(() => import("./pages/DashboardPages/SingleLocumPage"));
@@ -23,7 +23,64 @@ interface Props {
   children: React.ReactElement;
 }
 
+const dashboardChilren: RouteObject[] = [
+  {
+    path: "/dashboard",
+    element: <DashIndex />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard/open-locums",
+    element: <OpenLocums />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard/open-locums/:id",
+    element: <SingleLocumLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/dashboard/open-locums/:id",
+        element: <SingleLocumPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/dashboard/open-locums/:id/edit",
+        element: <LocumEdit />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard/booked-locums",
+    element: <BookedLocums />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard/booked-locums/:id",
+    element: <SingleLocumPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard/settled-locums",
+    element: <SettledLocums />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard/settled-locums/:id",
+    element: <SingleLocumPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard/create-new-locum",
+    element: <CreateNew />,
+    errorElement: <ErrorPage />,
+  },
+]
+
 function App(props: Props) {
+
+  
   const router = createBrowserRouter([
     {
       path: "/",
@@ -40,60 +97,7 @@ function App(props: Props) {
           path: "/dashboard",
           element: <LayoutDash />,
           errorElement: <ErrorPage />,
-          children: [
-            {
-              path: "/dashboard",
-              element: <DashIndex />,
-              errorElement: <ErrorPage />,
-            },
-            {
-              path: "/dashboard/open-locums",
-              element: <OpenLocums />,
-              errorElement: <ErrorPage />,
-            },
-            {
-              path: "/dashboard/open-locums/:id",
-              element: <SingleLocumLayout />,
-              errorElement: <ErrorPage />,
-              children: [
-                {
-                  path: "/dashboard/open-locums/:id",
-                  element: <SingleLocumPage />,
-                  errorElement: <ErrorPage />,
-                },
-                {
-                  path: "/dashboard/open-locums/:id/edit",
-                  element: <LocumEdit />,
-                  errorElement: <ErrorPage />,
-                },
-              ],
-            },
-            {
-              path: "/dashboard/booked-locums",
-              element: <BookedLocums />,
-              errorElement: <ErrorPage />,
-            },
-            {
-              path: "/dashboard/booked-locums/:id",
-              element: <SingleLocumPage />,
-              errorElement: <ErrorPage />,
-            },
-            {
-              path: "/dashboard/settled-locums",
-              element: <SettledLocums />,
-              errorElement: <ErrorPage />,
-            },
-            {
-              path: "/dashboard/settled-locums/:id",
-              element: <SingleLocumPage />,
-              errorElement: <ErrorPage />,
-            },
-            {
-              path: "/dashboard/create-new-locum",
-              element: <CreateNew />,
-              errorElement: <ErrorPage />,
-            },
-          ],
+          children: dashboardChilren
         },
         {
           path: "/login",
