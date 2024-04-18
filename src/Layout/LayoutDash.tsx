a1f88bb4e318029355b246c517dba490c719c4d3
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-18 18:45:19 +0300

    Refactored Dhasboard layout page

--- src/Layout/LayoutDash.tsx
+++ src/Layout/LayoutDash.tsx
@@ -1,32 +1,8 @@
-import { Breadcrumbs, Button, Link, LinkProps } from "@mui/material";
+import { Button } from "@mui/material";
 
-import {
-  Outlet,
-  Link as RouterLink,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import ProtectedRoute from "../components/ProtectedRoute";
 import RouterAnimation from "../pages/RouterAnimation";
-
-interface LinkRouterProps extends LinkProps {
-  to: string;
-  replace?: boolean;
-}
-interface RouteDisplay {
-  [key: string]: string
-}
-
-
-const RouteDisplay: RouteDisplay = {
-  "/dashboard/open-locums": "Open Locums",
-  "/dashboard/booked-locums": "Booked Locums",
-  "/dashboard/settled-locums": "Settled Locums",
-  "/dashboard/create-new-locum": "Create New Locum",
-};
-function LinkRouter(props: LinkRouterProps) {
-  return <Link {...props} component={RouterLink} />;
-}
 
 const LayoutDash = () => {
   const location = useLocation();
@@ -37,28 +13,7 @@ const LayoutDash = () => {
     <ProtectedRoute>
       <RouterAnimation>
         <div className="flex flex-col justify-start items-center h-full w-full py-[1rem]">
-          {!hideRegister && (
-            <>
-              <div className="w-[80%] mx-auto">
-                <Breadcrumbs separator=">">
-                  <LinkRouter
-                    to={`/dashboard`}
-                    underline="hover"
-                    color={hideRegister ? "purple" : "black"}
-                    sx={{ fontSize: "1.3rem", fontWeight: "bold" }}
-                  >
-                    Locums
-                  </LinkRouter>
-                  <LinkRouter
-                    to={`/dashboard/create-new-locum`}
-                    underline="hover"
-                    color={!hideRegister ? "purple" : "black"}
-                    sx={{ fontSize: "1.3rem", fontWeight: "bold" }}
-                  >
-                    {RouteDisplay[pathname as keyof RouteDisplay]}
-                  </LinkRouter>
-                </Breadcrumbs>
-              </div>
+            {!hideRegister && (
               <div className="w-[80%] mx-auto py-[0.4rem]">
                 <Button
                   color="info"
@@ -68,8 +23,8 @@ const LayoutDash = () => {
                   Back
                 </Button>
               </div>
-            </>
-          )}
+            )}
+          <div/>
           <Outlet />
         </div>
       </RouterAnimation>
