30bea2d8e0293f274f4117bb2526a71acc9320d8
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-19 12:01:54 +0300

    Made dashboard pages dynamic

--- src/pages/DashboardPages/CreateNewLocum.tsx
+++ src/pages/DashboardPages/CreateNewLocum.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import CreatedLocumNotification from "../../components/Dashboard/CreateLocums/CreatedLocumNotification";
 import { Job } from "../../components/Dashboard/CreateLocums/hooks/useJobForm";
 import NewLocumFormLayout from "../../components/Dashboard/CreateLocums/NewLocumFormLayout";
+import { useLocation } from "react-router-dom";
 const CreateNew = () => {
+  const { state } = useLocation();
+  document.title = state.title
   const [success, setSuccess] = useState<boolean>(false);
   const job: Job = {
     title: "",
