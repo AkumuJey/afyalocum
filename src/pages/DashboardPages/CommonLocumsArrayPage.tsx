3ee7c32eaf3a03439a1f6e3f77431a48bb68f47d
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-19 12:21:59 +0300

    Locums page error hadling working perfectly

--- src/pages/DashboardPages/CommonLocumsArrayPage.tsx
+++ src/pages/DashboardPages/CommonLocumsArrayPage.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "@mui/material";
+import { Skeleton, Typography } from "@mui/material";
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { SubmittedLocum } from "../../components/Dashboard/CreateLocums/hooks/useJobForm";
@@ -11,41 +11,52 @@ interface Status {
   completed: boolean;
 }
 
-
 const SettledLocums = () => {
   const { state } = useLocation();
-  document.title = state.title
+  document.title = state.title;
   const [locums, setLocums] = useState<SubmittedLocum[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
-  const { completed, booked} = state.status as Status
+  const { completed, booked } = state.status as Status;
+
   useEffect(() => {
+    const generateQuery = () => {
+      const locumsCollection = collection(db, "locums");
+      const openLocumsFilter = where("completed", "==", completed);
+      const bookedLocumsFilter = where("booked", "==", booked);
+      return query(locumsCollection, bookedLocumsFilter, openLocumsFilter);
+    };
     setLoading(true);
-    const locumsCollection = collection(db, "locums");
-    const openLocumsFilter = where("completed", "==", completed);
-    const bookedLocumsFilter = where("booked", "==", booked);
-    const openLocumsCollection = query(
-      locumsCollection,
-      bookedLocumsFilter,
-      openLocumsFilter
-    );
-    const unsubscribe = onSnapshot(openLocumsCollection, (snapshot) => {
-      const locumsArray: SubmittedLocum[] = [];
-      snapshot.docs.forEach((doc) => {
-        const data = doc.data() as SubmittedLocum;
-        data.id = doc.id;
-        if (!doc.data().closed) {
-          locumsArray.push(data);
-        }
-      });
-      setLoading(false);
-      setLocums([...locumsArray]);
+    const locumsCollection = generateQuery();
+    const unsubscribe = onSnapshot(locumsCollection, (snapshot) => {
+      try {
+        setLoading(true);
+        const locumsArray: SubmittedLocum[] = [];
+        snapshot.docs.forEach((doc) => {
+          const data = doc.data() as SubmittedLocum;
+          data.id = doc.id;
+          if (!doc.data().closed) {
+            locumsArray.push(data);
+          }
+        });
+        setError(false);
+        setLocums([...locumsArray]);
+      } catch (_error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     });
     return () => unsubscribe();
   }, [completed, booked]);
   return (
     <>
       <div className="flex gap-[1.5rem] flex-wrap p-[1.5rem] justify-start w-[95%] md:w-4/4 mx-auto">
+        {error && (
+          <Typography variant="h3" color="red">
+            An error occurred while fetching the data.
+          </Typography>
+        )}
         {loading ? (
           Array(3)
             .fill("key")
@@ -63,7 +74,9 @@ const SettledLocums = () => {
               </>
             ))
         ) : locums.length === 0 ? (
-          <p>No locums available</p>
+          <Typography variant="h3" color="red">
+            No locums available
+          </Typography>
         ) : (
           locums.map((locum) => <LocumCard key={locum.id} locum={locum} />)
         )}
