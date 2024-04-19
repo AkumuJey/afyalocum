e01793ff9d5805e58687cfc1867502c1e2c90580
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-19 23:12:23 +0300

    Error page settled

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,15 +1,38 @@
-import { Typography } from "@mui/material";
-import { useRouteError } from "react-router-dom"
-
+import { Button, Typography } from "@mui/material";
+import {
+  useNavigate,
+  useRouteError,
+} from "react-router-dom";
 
 const ErrorPage = () => {
-  const error = useRouteError()
+  const error = useRouteError();
   console.log(error);
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center valid-height py-[2rem] text-red-600">
-      <Typography variant="h3" fontWeight={` bold`}>Error 404</Typography>
+    <div className="flex flex-col items-center justify-center valid-height p-[2rem] gap-[3rem]">
+      <Typography variant="h5" fontWeight={` bold`} color={`red`}>
+        Error 404
+      </Typography>
+      <div className="flex justify-around w-full">
+        <Button
+          onClick={() => navigate("/")}
+          type="button"
+          variant="contained"
+          color="success"
+        >
+          Home
+        </Button>
+        <Button
+          onClick={() => navigate(-1)}
+          type="button"
+          variant="contained"
+          color="secondary"
+        >
+          Back
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
