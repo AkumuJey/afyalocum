afa9768588a1531cacb45d8ac2917e3ca867feb6
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-21 17:54:32 +0300

    Refactored registration page

---
+++ src/components/registration/VerificationSentComponent.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { Paper, Typography } from "@mui/material";
+
+const VerificationSentComponent = () => {
+  return (
+    <>
+      <Paper
+        elevation={3}
+        sx={{
+          p: 2,
+          width: "100%",
+          maxWidth: "500px",
+          mx: "auto",
+          background: "#DDD0C8",
+        }}
+      >
+        <div className="text-center">
+          <Typography variant="h6">Verification Link to Your email.</Typography>
+          <Typography variant="h6">Please check your inbox.</Typography>
+        </div>
+        <div className="mt-4 flex justify-evenly">
+          <Link
+            to={`/`}
+            className="py-[0.5rem] px-[1.5rem] rounded-md bg-teal-600 hover:bg-teal-800 hover:text-white font-semibold text-xl"
+          >
+            Home
+          </Link>
+          <Link
+            to={`/login`}
+            className="py-[0.5rem] px-[1.5rem] rounded-md bg-purple-600 hover:bg-purple-900 hover:text-white font-semibold text-xl"
+          >
+            Login
+          </Link>
+        </div>
+      </Paper>
+    </>
+  );
+};
+
+export default VerificationSentComponent;
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,9 +1,9 @@
 import { useContext, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import RegistrationFormLayout from "../components/Registration/RegistrationFormLayourt";
+import VerificationSentComponent from "../components/Registration/VerificationSentComponent";
 import { AuthContext } from "../contexts/AuthContext";
 import RouterAnimation from "./RouterAnimation";
-import RegistrationFormLayout from "../components/Registration/RegistrationFormLayourt";
-import { Paper, Typography } from "@mui/material";
 
 const Resigstration = () => {
   document.title = "AfyaLocum - Signup";
@@ -16,19 +16,12 @@ const Resigstration = () => {
   return (
     <>
       <RouterAnimation>
-        {sent && (
-          <Paper elevation={3} sx={{ p: 2 , width: "100%", maxWidth: "500px", mx: "auto", background: "#DDD0C8"}}>
-            <div className="text-center">
-              <Typography variant="h6">Verification Link to Your email.</Typography>
-              <Typography variant="h6">Please check your inbox.</Typography>
-            </div>
-            <div className="mt-4 flex justify-evenly">
-              <Link to={`/`} className="py-[0.5rem] px-[1.5rem] rounded-md bg-teal-600 hover:bg-teal-800 hover:text-white font-semibold text-xl">Home</Link>
-              <Link to={`/login`} className="py-[0.5rem] px-[1.5rem] rounded-md bg-purple-600 hover:bg-purple-900 hover:text-white font-semibold text-xl">Login</Link>
-            </div>
-          </Paper>
+        {sent && <VerificationSentComponent />}
+        {!sent && (
+          <RegistrationFormLayout
+            notifyVerificationSent={() => setSent(true)}
+          />
         )}
-        {!sent && <RegistrationFormLayout notifyVerificationSent={() => setSent(true)} />}
       </RouterAnimation>
     </>
   );
