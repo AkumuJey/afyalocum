9e769d1772df15fb88c1e6a1bd1105070c171f93
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-25 21:34:49 +0300

    Working array

--- src/components/Home/SponsorsAndApprovals/SponsorsAndApprovalsArrayDisplay.tsx
+++ src/components/Home/SponsorsAndApprovals/SponsorsAndApprovalsArrayDisplay.tsx
@@ -1,58 +1,29 @@
 import { Button } from "@mui/material";
-import { motion } from "framer-motion";
 
 const SponsorsAndApprovalsArrayDisplay = () => {
-  const array = new Array(5).fill("sponsor");
+  const array = new Array(10).fill("sponsor");
   return (
     <>
-      <div id="horizontal-scroll" className="py-2 w-full max-w-xl mx-auto overflow-hidden flex flex-nowrap gap-2 ">
-        <motion.ul
-          className="flex flex-row flex-nowrap gap-2"
-          initial={{ x: 0 }}
-          animate={{ x: "-100%" }}
-          transition={{ duration: 10, ease: "linear", repeat: Infinity }}
-        >
+      <div className="slide-container">
+        <ul className="slide">
           {array.map((sample, i) => (
             <li key={i + 1}>
-              <Button variant="contained" color="success">{sample}</Button>
+              <Button variant="contained" color="success">
+                {sample}
+              </Button>
             </li>
           ))}
-        </motion.ul>
-        <motion.ul
-          className="flex flex-row flex-nowrap gap-2"
-          initial={{ x: 0 }}
-          animate={{ x: "-100%" }}
-          transition={{ duration: 10, ease: "linear", repeat: Infinity }}
-        >
+        </ul>
+        <ul className="slide">
           {array.map((sample, i) => (
             <li key={i + 1}>
-              <Button variant="contained" color="success">{sample}</Button>
+              <Button variant="contained" color="success">
+                {sample}
+              </Button>
             </li>
           ))}
-        </motion.ul>
+        </ul>
       </div>
-      {/* <div className="bg-green-500 overflow-hidden inline-block whitespace-nowrap">
-        <motion.div
-          initial={{ x: 0 }}
-          animate={{ x: "100%" }}
-          transition={{ duration: 30, repeat: Infinity }}
-          className="flex flex-nowrap justify-around my-[0.5rem] gap-[1rem]"
-        >
-          {array.map((element, index) => (
-            <SponsorOrApprovalComponent key={element + index} />
-          ))}
-        </motion.div>
-        <motion.div
-          initial={{ x: 0 }}
-          animate={{ x: "100%" }}
-          transition={{ duration: 30, repeat: Infinity }}
-          className="flex flex-nowrap justify-around my-[0.5rem] gap-[1rem]"
-        >
-          {array.map((element, index) => (
-            <SponsorOrApprovalComponent key={element + index} />
-          ))}
-        </motion.div>
-      </div> */}
     </>
   );
 };
